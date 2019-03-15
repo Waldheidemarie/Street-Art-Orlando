0f38192de6d917b2936ea2a97274706e4e330d7a
Eric Good <ericdgood@yahoo.com>
2019-03-15 15:01:41 -0400

    Intent from image to details layout

--- app/src/main/java/streetart/CFO/orlandostreetart/adapters/MainRecyclerviewAdapter.java
+++ app/src/main/java/streetart/CFO/orlandostreetart/adapters/MainRecyclerviewAdapter.java
@@ -1,7 +1,6 @@
 package streetart.CFO.orlandostreetart.adapters;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -15,6 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import streetart.CFO.orlandostreetart.R;
 import streetart.CFO.orlandostreetart.models.GetSubmissions;
+import streetart.CFO.orlandostreetart.views.Details;
 import streetart.CFO.orlandostreetart.views.ExploreFragment;
 
 /**
@@ -22,8 +22,8 @@ import streetart.CFO.orlandostreetart.views.ExploreFragment;
  */
 public class MainRecyclerviewAdapter extends RecyclerView.Adapter<MainRecyclerviewAdapter.ViewHolder> {
 
-    GetSubmissions itemsData;
-    ExploreFragment context;
+    private GetSubmissions itemsData;
+    private ExploreFragment context;
 
 
     public MainRecyclerviewAdapter(ExploreFragment exploreFragment, GetSubmissions itemsData) {
@@ -33,13 +33,18 @@ public class MainRecyclerviewAdapter extends RecyclerView.Adapter<MainRecyclervi
 
     @Override
     public void onBindViewHolder(@NonNull MainRecyclerviewAdapter.ViewHolder viewHolder, int position) {
-//        viewHolder.mImageView.setImageResource();
-
-
 //         Load Image
         Glide.with(context)
                 .load(itemsData.getSubmissions().get(position).getThumbUrl())
                 .into(viewHolder.mImageView);
+
+        viewHolder.mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openDetails = new Intent(v.getContext() , Details.class);
+                context.startActivity(openDetails);
+            }
+        });
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
---
+++ app/src/main/java/streetart/CFO/orlandostreetart/views/Details.java
@@ -0,0 +1,19 @@
+package streetart.CFO.orlandostreetart.views;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import streetart.CFO.orlandostreetart.R;
+
+/**
+ * Created by Eric on 3/15/2019.
+ */
+public class Details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details);
+    }
+}
