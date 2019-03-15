59b88cd1b3a43cbc0833e8cafbbae4c0d5c3bebc
Eric Good <ericdgood@yahoo.com>
2019-03-15 14:00:01 -0400

    Hide API key

--- app/src/main/java/streetart/CFO/orlandostreetart/network/GetNetworkData.java
+++ app/src/main/java/streetart/CFO/orlandostreetart/network/GetNetworkData.java
@@ -3,6 +3,7 @@ package streetart.CFO.orlandostreetart.network;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import streetart.CFO.orlandostreetart.BuildConfig;
 import streetart.CFO.orlandostreetart.R;
 import streetart.CFO.orlandostreetart.models.GetSubmissions;
 
@@ -10,7 +11,7 @@ public interface GetNetworkData {
 
     @Headers({"Accept: application/json",
             "Content-Type: application/json",
-    "Authorization: " + R.string.API_KEY})
+    "Authorization: " + BuildConfig.API_KEY})
     @GET("submissions?status=approved")
     Call<GetSubmissions> getSubmissions();
 
